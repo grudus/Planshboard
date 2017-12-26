2b2e5482af76e801d23acf5f739209e881967a5c
grudus <jakgruda@gmail.com>
2017-12-26 13:36:16 +0100

    Disble create user when username exists

--- src/main/kotlin/com/grudus/planshboard/commons/Keys.kt
+++ src/main/kotlin/com/grudus/planshboard/commons/Keys.kt
@@ -3,4 +3,5 @@ package com.grudus.planshboard.commons
 object Keys {
     val EMPTY_USERNAME = "USERNAME.EMPTY"
     val EMPTY_PASSWORD = "PASSWORD.EMPTY"
+    val USERNAME_EXISTS = "USERNAME.EXISTS"
 }

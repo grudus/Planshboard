125c1ddfd569c5b59497d87ba1e7b9b6bb3af045
grudus <jakgruda@gmail.com>
2018-02-11 21:24:54 +0100

    Fix tests

--- src/main/kotlin/com/grudus/planshboard/games/opponent/Opponent.kt
+++ src/main/kotlin/com/grudus/planshboard/games/opponent/Opponent.kt
@@ -2,4 +2,4 @@ package com.grudus.planshboard.games.opponent
 
 import com.grudus.planshboard.commons.Id
 
-class Opponent(var id: Id?, val userId: Id, val name: String, val isRealUser: Boolean)
+class Opponent(var id: Id?, val userId: Id, val name: String, val isRealUser: Boolean? = false)

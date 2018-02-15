854434da60b3bcdb1af1100395ba1dbd08389299
grudus <jakgruda@gmail.com>
2018-02-15 18:30:23 +0100

    Add secondary constructor for Opponent (fix jooq)

--- src/main/kotlin/com/grudus/planshboard/games/opponent/Opponent.kt
+++ src/main/kotlin/com/grudus/planshboard/games/opponent/Opponent.kt
@@ -2,4 +2,6 @@ package com.grudus.planshboard.games.opponent
 
 import com.grudus.planshboard.commons.Id
 
-class Opponent(var id: Id?, val userId: Id, val name: String, val isRealUser: Boolean? = false)
+class Opponent(var id: Id?, val userId: Id, val name: String, val isRealUser: Boolean) {
+    constructor(id: Id?, userId: Id, name: String): this(id, userId, name, false)
+}

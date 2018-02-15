55d983ee1035837b6054731d9ac71b095edbdb16
grudus <jakgruda@gmail.com>
2018-02-15 18:31:46 +0100

    Suppress unused warning for Opponent

--- src/main/kotlin/com/grudus/planshboard/games/opponent/Opponent.kt
+++ src/main/kotlin/com/grudus/planshboard/games/opponent/Opponent.kt
@@ -3,5 +3,6 @@ package com.grudus.planshboard.games.opponent
 import com.grudus.planshboard.commons.Id
 
 class Opponent(var id: Id?, val userId: Id, val name: String, val isRealUser: Boolean) {
+    @Suppress("unused") //Jooq uses it
     constructor(id: Id?, userId: Id, name: String): this(id, userId, name, false)
 }

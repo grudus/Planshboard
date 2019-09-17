11a63485b6e4c82428fe0f178f50113acfc10ce8
grudus <jakgruda@gmail.com>
2019-09-17 20:41:41 +0200

    Fix updating sql

--- src/main/resources/db/migration/V1_9__change_opponents_structure_2.sql
+++ src/main/resources/db/migration/V1_9__change_opponents_structure_2.sql
@@ -1,4 +1,4 @@
+-- noinspection SqlWithoutWhere
 UPDATE opponents
 SET created_by = user_id,
-pointing_to_user = (CASE WHEN is_real_user THEN user_id ELSE null END)
-FROM opponents;
+pointing_to_user = (CASE WHEN is_real_user THEN user_id ELSE null END);
